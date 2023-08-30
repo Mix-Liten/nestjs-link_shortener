800668bd9b5dd09a8f16db3a9c1850da24c31beb
Mix-Liten <mixliten@gmail.com>
2023-08-30 16:25:27 +0800

    use redis in runtime

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AppRepositoryTag } from './app.repository'
-import { AppRepositoryHashmap } from './app.repository.hashmap'
+import { AppRepositoryRedis } from './app.repository.redis'
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, { provide: AppRepositoryTag, useClass: AppRepositoryHashmap }],
+  providers: [AppService, { provide: AppRepositoryTag, useClass: AppRepositoryRedis }],
 })
 export class AppModule {}
